5e08140ffee26da457ecb93ba712912a4113a5d8
marco.dellovo <marco.dellovo@docebo.com>
2021-07-06 10:59:45 +0200

    Initial commit

--- src/stories/Bug.stories.ts
+++ src/stories/Bug.stories.ts
@@ -1,3 +1,17 @@
-/**
- * @author: Marco Dell'Ovo
- */
+// also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import Bug from './bug.component';
+
+export default {
+    title: 'Bug',
+    component: Bug,
+} as Meta;
+
+const Template: Story<Bug> = (args: Bug) => ({
+    props: args,
+});
+
+export const BugCase = Template.bind({});
+BugCase.args = {
+    correctAnswers: [],
+};
--- src/stories/bug.component.ts
+++ src/stories/bug.component.ts
@@ -1,3 +1,9 @@
-/**
- * @author: Marco Dell'Ovo
- */
+import {Component, Input} from "@angular/core";
+
+@Component({
+    selector: 'storybook-bug',
+    template: `<div>{{correctAnswers.join()}}</div>`,
+})
+export default class BugComponent {
+    @Input() correctAnswers: string[] = [];
+}
